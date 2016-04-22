1db0802f2b6e70ab17db5ee59d1786f7582764a9
vitac215 <vitac215@gmail.com>
2016-04-22 18:07:34 -0500

    no message

--- js/budget.js
+++ js/budget.js
@@ -183,7 +183,39 @@ $(function(){
 
 
 	// Register
-	
+	function registerAjax(event) {
+		var username = document.getElementById("username").value; // Get the username from the form
+		var password = document.getElementById("password").value; // Get the password from the form
+	 
+		// Make a URL-encoded string for passing POST data:
+		var dataString = "username=" + encodeURIComponent(username) + "&password=" + encodeURIComponent(password);
+	 
+		var xmlHttp = new XMLHttpRequest(); // Initialize our XMLHttpRequest instance
+		xmlHttp.open("POST", "register_ajax.php", true); // Starting a POST request (NEVER send passwords as GET variables!!!)
+		xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); // It's easy to forget this line for POST requests
+		xmlHttp.addEventListener("load", function(event){
+			var jsonData = JSON.parse(event.target.responseText); // parse the JSON into a JavaScript object
+			if(jsonData.success){  // in PHP, this was the "success" key in the associative array; in JavaScript, it's the .success property of jsonData
+				alert("You've been registered successfully!");
+
+				// // Change the login status
+				// $("#loginStatus").html("Welcome, " + username);
+
+				// // Display the evnets
+				// displayEvent();
+
+			}else{
+				alert("You were not registered:  "+jsonData.message);
+			}
+		}, false); // Bind the callback to the load event
+		xmlHttp.send(dataString); // Send the data
+	}
+
+	$("#btn_reg").click(registerAjax);
+	// document.getElementById("btn_reg").addEventListener("click", registerAjax, false); // Bind the AJAX call to button click
+
+
+	// 
 
 
 
